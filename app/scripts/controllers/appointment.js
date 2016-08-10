84b587efea729abac75a4887e3bc7765dbf3fe65
Apurv Kulkarni <apurvmk@gmail.com>
2016-08-10 06:41:18 -0400

    WSCW-1 Added appointment

---
+++ app/scripts/controllers/appointment.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yapp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of yapp
+ */
+
+// define angular module/app
+var formApp = angular.module('yapp').controller('FormCtrl', function ($scope, $http) {
+			
+			// create a blank object to hold our form information
+			// $scope will allow this to pass between controller and view
+			$scope.formData = {};
+			 var config = {
+                headers : { 
+                			'Content-Type': 'application/json' , 
+                			'patientId': '$scope.formData.patientId', 
+                			'doctorId': '$scope.formData.doctorId'
+                		} 
+            };
+			// process the form
+			$scope.appointment = function() {
+				$http.post('https://4kd2ksjc12.execute-api.us-east-1.amazonaws.com/prod/patient/appointment',
+					JSON.stringify({"appointmentSubject":$scope.formData.appointmentSubject, "appointmentDescription": $scope.formData.appointmentDescription, "appointmentDateTime": $scope.formData.appointmentDateTime.toISOString()})
+					,config)
+			        .success(function(data) {
+			            console.log(data);
+			            if (!data.success) {
+			            	// if not successful, bind errors to error variables
+			                $scope.errorName = data.errors.name;
+			                $scope.errorSuperhero = data.errors.superheroAlias;
+			            } else {
+			            	// if successful, bind success message to message
+			                $scope.message = data.message;
+                                        $scope.errorName = '';
+			                $scope.errorSuperhero = '';
+			            }
+			        });
+			};
+
+});
+
