5f167f3974d459a245c23916eebb8842069cb3d1
Apurv Kulkarni <apurvmk@gmail.com>
2016-08-10 10:48:42 -0400

    WSCW-1 Completed scheduling appointment

--- app/scripts/controllers/appointment.js
+++ app/scripts/controllers/appointment.js
@@ -14,15 +14,15 @@ var formApp = angular.module('yapp').controller('FormCtrl', function ($scope, $h
 			// create a blank object to hold our form information
 			// $scope will allow this to pass between controller and view
 			$scope.formData = {};
-			 var config = {
-                headers : { 
-                			'Content-Type': 'application/json' , 
-                			'patientId': '$scope.formData.patientId', 
-                			'doctorId': '$scope.formData.doctorId'
-                		} 
-            };
+
+             
 			// process the form
 			$scope.appointment = function() {
+			 	var config = {
+                headers : { 'Content-Type': 'application/json' }}; 
+            	config.headers.patientId = $scope.formData.patientId;
+            	config.headers.doctorId = $scope.formData.doctorId;
+
 				$http.post('https://4kd2ksjc12.execute-api.us-east-1.amazonaws.com/prod/patient/appointment',
 					JSON.stringify({"appointmentSubject":$scope.formData.appointmentSubject, "appointmentDescription": $scope.formData.appointmentDescription, "appointmentDateTime": $scope.formData.appointmentDateTime.toISOString()})
 					,config)
